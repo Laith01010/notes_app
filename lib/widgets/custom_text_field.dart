17c3a034471c591daed98cf29ba9ae64cf9a3289
Laith01010 <laith.storm2002@hotmail.com>
2026-05-03 12:51:33 +0200

    editing note

--- lib/widgets/custom_text_field.dart
+++ lib/widgets/custom_text_field.dart
@@ -7,14 +7,17 @@ class CustomTextField extends StatelessWidget {
     required this.hint,
     this.maxLines = 1,
     this.onSaved,
+    this.onChanged,
   });
 
   final String hint;
   final int maxLines;
+  final Function(String)? onChanged;
   final void Function(String?)? onSaved;
   @override
   Widget build(BuildContext context) {
     return TextFormField(
+      onChanged: onChanged,
       onSaved: onSaved,
       validator: (value) {
         if (value?.isEmpty ?? true) {
